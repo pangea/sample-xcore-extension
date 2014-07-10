5d6d734299400ad4eba17cdfb6a8a6fd46366608
Mark McCanna <markmccanna1@gmail.com>
2014-07-10 15:19:36 -0500

    fixed datalist

--- client/views/workspace.js
+++ client/views/workspace.js
@@ -1,18 +1,27 @@
 enyo.kind({
   name: "XV.SampleExtensionWorkspace", 
-  kind: "enyo.DataList",
   fit: true,
+  events: {
+    onAlert: ""
+  },
   components: [
-    {classes: "item-holder", components: [
-      {name: "item"},
-      {name: "item2"},
-      {name: "item3"}
-    ],
-     bindings: [
-       {from: ".model.name", to: ".$.item.content"},
-       {from: ".model.name2", to: ".$.item2.content"},
-       {from: ".model.name3", to: ".$.item3.content"}
-     ]}
+    {name: 'list', kind: 'enyo.DataList', components: [
+      {components: [
+        {classes: "item-holder", components: [
+          {name: "item"},
+          {name: "item2"},
+          {name: "item3"}
+        ]}
+      ], 
+       bindings: [
+         {from: ".model.name", to: ".$.item.content"},
+         {from: ".model.name2", to: ".$.item2.content"},
+         {from: ".model.name3", to: ".$.item3.content"}
+       ], ontap: 'testAlert'}
+    ]}
+  ],
+  bindings: [
+    {from : '.collection', to: '.$.list.collection'}
   ],
   create: enyo.inherit(function (sup) {
     return function () {
@@ -21,6 +30,11 @@ enyo.kind({
     };
   }),
   data: [
-    new XM.SampleModel({name: "This is a test workspace", name2: "This is a test workspace", name3: "This is a test workspace"})
-  ]
+   {name: "This is a test workspace", name2: "This is a test workspace", name3: "This is a test workspace"}
+  ],
+  testAlert: function(inSender, inEvent) {
+    alert = {content: 'warning', type: 'warning'};
+    this.doAlert(alert);
+  }
 });
+
